6e657faf8a339e903a5da7772c48f004e249456f
Kirill Krolik <yoursuffer@gmail.com>
2017-10-23 16:55:51 +0300

    fix compatibility with PHPSpec 3

--- src/M00t/PhpSpec/PHPUnitMatchers/Matcher/IdentityMatcher.php
+++ src/M00t/PhpSpec/PHPUnitMatchers/Matcher/IdentityMatcher.php
@@ -4,8 +4,71 @@
 namespace M00t\PhpSpec\PHPUnitMatchers\Matcher;
 
 
-class IdentityMatcher extends \PhpSpec\Matcher\IdentityMatcher
+use PhpSpec\Exception\Example\FailureException;
+use PhpSpec\Exception\Example\NotEqualException;
+use PhpSpec\Formatter\Presenter\Presenter;
+use PhpSpec\Matcher\BasicMatcher;
+
+class IdentityMatcher extends BasicMatcher
 {
+
+    private static $keywords = array(
+        'return',
+        'be',
+        'equal',
+        'beEqualTo',
+    );
+
+    private $presenter;
+
+    public function __construct(Presenter $presenter)
+    {
+        $this->presenter = $presenter;
+    }
+
+    /**
+     * @param string $name
+     * @param mixed  $subject
+     * @param array  $arguments
+     *
+     * @return bool
+     */
+    public function supports($name, $subject, array $arguments)
+    {
+        return in_array($name, self::$keywords) && 1 == count($arguments);
+    }
+
+    /**
+     * @param string $name
+     * @param mixed  $subject
+     * @param array  $arguments
+     *
+     * @return NotEqualException
+     */
+    protected function getFailureException($name, $subject, array $arguments)
+    {
+        return new NotEqualException(sprintf(
+            'Expected %s, but got %s.',
+            $this->presenter->presentValue($arguments[0]),
+            $this->presenter->presentValue($subject)
+        ), $arguments[0], $subject);
+    }
+
+    /**
+     * @param string $name
+     * @param mixed  $subject
+     * @param array  $arguments
+     *
+     * @return FailureException
+     */
+    protected function getNegativeFailureException($name, $subject, array $arguments)
+    {
+        return new FailureException(sprintf(
+            'Did not expect %s, but got one.',
+            $this->presenter->presentValue($subject)
+        ));
+    }
+
     protected function matches($subject, array $arguments)
     {
         try {
